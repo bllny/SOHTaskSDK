d1b65be20bb947d8c3e8b4d4e079b2bd25bf6095
jiangzhou <jiangzhoubai@gmail.com>
2016-08-30 17:50:05 +0800

    0.2.2

---
+++ SohouerSDK/SohouerSDK.framework/Headers/SOHTaskSDKDelegate.h
@@ -0,0 +1,32 @@
+//
+//  SOHTaskSDKDelegate.h
+//  Sohouer
+//
+//  Created by BaiJiangzhou on 16/8/30.
+//  Copyright © 2016年 com.sohouer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// api回调
+@protocol SHEAPIManagerApiCallBackDelegate <NSObject>
+@required
+- (void)managerCallAPIDidSuccess:(id)manager;
+- (void)managerCallAPIDidFailed:(id)manager;
+@end
+
+@protocol SHETaskDownloadDelegate <NSObject>
+
+@optional
+- (void) downloaded:(id) task;
+- (void) progress:(id) task progress:(CGFloat)progress;
+- (void) downloading:(id) task;
+- (void) open:(id) task result:(BOOL)result;
+- (void) opened:(id) task;
+- (void) finish:(id) task;
+- (void) cancel:(id) task;
+
+@end
+
+
+
